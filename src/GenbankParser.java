266e79f25592646eae810ce4ee97bd4e6ddac6de
JudithNeulamm <judith.neukamm@gmx.net>
2017-03-20 20:06:34 +0000

    update parser: list all genes

--- src/GenbankParser.java
+++ src/GenbankParser.java
@@ -1,5 +1,27 @@
+import org.renci.gbff.GBFFManager;
+import org.renci.gbff.model.Feature;
+import org.renci.gbff.model.Sequence;
+
+import java.io.File;
+import java.util.List;
+
 /**
  * Created by neukamm on 20.03.17.
  */
 public class GenbankParser {
+
+    public void read(String filepath){
+
+        GBFFManager parser = GBFFManager.getInstance();
+        List<Sequence> results = parser.deserialize(new File(filepath));
+        for (Sequence seq : results){
+            for(Feature f : seq.getFeatures()){
+                if(f.getType().equals("gene"))
+                    System.out.println(f.getQualifiers().get("gene"));
+            }
+        }
+
+    }
+
+
 }
