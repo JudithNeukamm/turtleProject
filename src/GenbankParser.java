1d15d2fca3b0b8e1dd3d703b237585155364d545
JudithNeulamm <judith.neukamm@gmx.net>
2017-03-21 08:31:40 +0000

    add genelist parser

--- src/GenbankParser.java
+++ src/GenbankParser.java
@@ -2,7 +2,10 @@ import org.renci.gbff.GBFFManager;
 import org.renci.gbff.model.Feature;
 import org.renci.gbff.model.Sequence;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -10,10 +13,50 @@ import java.util.List;
  */
 public class GenbankParser {
 
-    public void read(String filepath){
+    public void readGenelist(String filepath_genelist){
+
+        BufferedReader br = null;
+        FileReader fr = null;
+
+        try {
+
+            fr = new FileReader(filepath_genelist);
+            br = new BufferedReader(fr);
+
+            String sCurrentLine;
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                System.out.println(sCurrentLine);
+            }
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            try {
+
+                if (br != null)
+                    br.close();
+
+                if (fr != null)
+                    fr.close();
+
+            } catch (IOException ex) {
+
+                ex.printStackTrace();
+
+            }
+
+        }
+
+    }
+
+    public void readGenbankFile(String filepath_genbank){
 
         GBFFManager parser = GBFFManager.getInstance();
-        List<Sequence> results = parser.deserialize(new File(filepath));
+        List<Sequence> results = parser.deserialize(new File(filepath_genbank));
         for (Sequence seq : results){
             for(Feature f : seq.getFeatures()){
                 if(f.getType().equals("gene"))
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
        GenbankParser genbankParser = new GenbankParser();
-       genbankParser.read(args[0]);
+       genbankParser.readGenelist(args[0]);
+       genbankParser.readGenbankFile(args[1]);
     }
 }
